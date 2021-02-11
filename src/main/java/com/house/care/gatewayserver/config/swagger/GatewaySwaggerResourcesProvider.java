6178e9adc7011741960f73874325f10ed9058060
Ernani Ramos <ernaniramos@gmail.com>
2021-02-11 12:11:03 +0000

    Refactory and add sleuth

--- src/main/java/com/house/care/gatewayserver/config/swagger/GatewaySwaggerResourcesProvider.java
+++ src/main/java/com/house/care/gatewayserver/config/swagger/GatewaySwaggerResourcesProvider.java
@@ -2,13 +2,16 @@ package com.house.care.gatewayserver.config.swagger;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.netflix.zuul.filters.Route;
 import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import com.google.common.collect.Lists;
+
+import lombok.RequiredArgsConstructor;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
@@ -17,35 +20,24 @@ import springfox.documentation.swagger.web.SwaggerResourcesProvider;
  */
 @Component
 @Primary
+@RequiredArgsConstructor
 public class GatewaySwaggerResourcesProvider implements SwaggerResourcesProvider {
 
     private final RouteLocator routeLocator;
 
-    private final DiscoveryClient discoveryClient;
-
-    public GatewaySwaggerResourcesProvider(RouteLocator routeLocator, DiscoveryClient discoveryClient) {
-
-        this.routeLocator = routeLocator;
-        this.discoveryClient = discoveryClient;
-    }
-
     @Override
     public List<SwaggerResource> get() {
-
-        List<SwaggerResource> resources = new ArrayList<>();
-
         // Add the default swagger resource that correspond to the gateway's own
         // swagger doc
-        resources.add(swaggerResource("default", "/v3/api-docs"));
 
         // Add the registered microservices swagger docs as additional swagger
         // resources
-        List<Route> routes = routeLocator.getRoutes();
-        routes.forEach(route -> {
-            resources.add(swaggerResource(route.getId(), route.getFullPath().replace("**", "v3/api-docs")));
-        });
-
-        return resources;
+        return Optional.ofNullable(routeLocator.getRoutes()).map(routes1 -> {
+            List<SwaggerResource> resources = Lists.newArrayList(swaggerResource("default", "/v3/api-docs"));
+            resources
+                    .addAll(routes1.stream().map(route -> swaggerResource(route.getId(), route.getFullPath().replace("**", "v3/api-docs"))).collect(Collectors.toList()));
+            return resources;
+        }).orElseGet(ArrayList::new);
     }
 
     private SwaggerResource swaggerResource(String name, String location) {
--- src/main/java/com/house/care/gatewayserver/web/rest/ServiceDefinitionController.java
+++ src/main/java/com/house/care/gatewayserver/web/rest/ServiceDefinitionController.java
@@ -1,6 +1,5 @@
 package com.house.care.gatewayserver.web.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.house.care.gatewayserver.config.swagger.ServiceDefinitionsContext;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class ServiceDefinitionController {
 
-    @Autowired
-    private ServiceDefinitionsContext definitionContext;
+    private final ServiceDefinitionsContext definitionContext;
 
-    @GetMapping("/service/{servicename}")
-    public String getServiceDefinition(@PathVariable("servicename") String serviceName) {
+    @GetMapping("/service/{serviceName}")
+    public String getServiceDefinition(@PathVariable("serviceName") String serviceName) {
 
         return definitionContext.getSwaggerDefinition(serviceName);
     }
